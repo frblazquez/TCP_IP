cc21ca286ec8f7084beeb7efe1fc7c950f008c7c
leoalz <71744574+leoalz@users.noreply.github.com>
2020-10-12 00:37:25 +0200

    lanTopology.py modified added to scripts foldr

--- Lab 1/scripts/lanTopology.py
+++ Lab 1/scripts/lanTopology.py
@@ -19,12 +19,25 @@ def firstNetwork():
 
     info( '*** Adding hosts\n' )
     PC1 = net.addHost( 'PC1')
-    
+    PC2 = net.addHost( 'PC2')
+    PC3 = net.addHost( 'PC3')
+    PC4 = net.addHost( 'PC4')
+
     info( '*** Adding switch\n' )
     s14 = net.addSwitch( 's14' )
+    s24 = net.addSwitch( 's24' )
+    s34 = net.addSwitch( 's34' )
+
     
     info( '*** Creating links\n' )
     net.addLink( PC1, s14 )
+    net.addLink( PC2, s24 )
+    net.addLink( PC3, s34 )
+    net.addLink( PC4, s14 )
+    net.addLink( PC4, s24 )
+    net.addLink( PC4, s34 )
+
+
     
     info( '*** Starting network\n')
     net.start()
